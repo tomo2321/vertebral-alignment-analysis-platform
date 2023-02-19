2dd3ef4d3b127ec54fc46ff8b24a2d39c2334e53
hakot <tomo2321@gmail.com>
2023-02-19 21:54:28 +0900

    [update] visualization bash script for offscreen rendering

--- landmark_segmentation_uncertainty/test/visualization_all.sh
+++ landmark_segmentation_uncertainty/test/visualization_all.sh
@@ -1,1 +1,19 @@
-python ../volume-renderer-master/render_with_all_results_arg.py --img_folder ./img --seg_folder ./results/vertebrae_bayesian_segmentation_original --csv_folder ./results/vertebrae_localization --vis_folder ./visualization
+#!/bin/bash
+
+cd $(realpath ${0} | xargs dirname)
+
+IMG='./img'
+MODEL='verse2019_jmid_1100cases'
+RESULTS="./results/${MODEL}"
+VISUALIZATION='./visualization'
+
+export DISPLAY=:99.0
+Xvfb ${DISPLAY} -screen 0 1024x768x24 > /dev/null 2>&1 &
+sleep 3
+exec "$@"
+
+python ../volume-renderer-master/render_with_all_results_arg.py \
+  --img_folder ${IMG} \
+  --seg_folder ${RESULTS}/vertebrae_bayesian_segmentation_original \
+  --csv_folder ${RESULTS}/vertebrae_localization \
+  --vis_folder ${VISUALIZATION}
